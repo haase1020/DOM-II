67f8d5e6fb406e2ee1e68f273f37018b27b18dbf
Mandi Haase <haase1020@gmail.com>
2020-01-14 15:42:39 -0500

    add events to index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+const logo = 
+document.querySelector(".logo-heading")
+console.log(logo);
+logo.addEventListener ("mouseenter", ()=>{
+    logo.style.transform = "scale(1.4)";
+})
+logo.addEventListener("mouseleave", ()=>{
+logo.style.transform = "scale(1)";
+})
+
+
+function zoom(event) {
+    event.preventDefault();
+  
+    scale += event.deltaY * -0.01;
+  
+    // Restrict scale
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    // Apply scale transform
+    header.style.transform = `scale(${scale})`;
+  }
+  
+  let scale = 1;
+  const header = document.querySelector(".intro")
+header.onwheel = zoom;
+ 
